efdf9d6fe7755415662631275f21e09c4e37257a
niitagile <43024343+niitagile@users.noreply.github.com>
2022-08-08 10:45:43 +0530

    Add files via upload

---
+++ 08Aug/FirstHibernateAnnotationProject/src/main/java/com/StudentInfo/Student.java
@@ -0,0 +1,38 @@
+package com.StudentInfo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="Student")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int rollno;
+	@Column(length=20)
+	private String name;
+	@Column
+	private int marks;
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
