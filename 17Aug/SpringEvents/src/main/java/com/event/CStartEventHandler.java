60580166068bcd7eaf4be3128ea7be6747b8b1b8
niitagile <43024343+niitagile@users.noreply.github.com>
2022-08-17 10:05:20 +0530

    Add files via upload

---
+++ 17Aug/SpringEvents/src/main/java/com/event/CStartEventHandler.java
@@ -0,0 +1,13 @@
+package com.event;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextStartedEvent;
+
+public class CStartEventHandler 
+   implements ApplicationListener<ContextStartedEvent>{
+
+	public void onApplicationEvent(ContextStartedEvent event) {
+	      System.out.println("ContextStartedEvent Received");
+	   }
+
+   
+}
