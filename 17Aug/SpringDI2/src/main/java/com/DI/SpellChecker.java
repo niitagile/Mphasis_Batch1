22d28e7e4fbc762fea74f5c5cb27dba41655476b
niitagile <43024343+niitagile@users.noreply.github.com>
2022-08-17 09:29:09 +0530

    Add files via upload

---
+++ 17Aug/SpringDI2/src/main/java/com/DI/SpellChecker.java
@@ -0,0 +1,12 @@
+package com.DI;
+
+
+public class SpellChecker {
+	public SpellChecker(){
+		System.out.println("Spellchecker Construtor");
+	}
+	public void checkSpelling(){
+		System.out.println("checking is going on");
+	}
+
+}
---
+++ 17Aug/SpringDI2/src/main/java/com/DI/TextEditor.java
@@ -0,0 +1,23 @@
+package com.DI;
+
+
+public class TextEditor {
+	private SpellChecker sp;
+	
+	
+	public TextEditor(){
+	
+	}
+	public TextEditor(SpellChecker sp) {
+		super();
+		this.sp = sp;
+	}
+	public void spellCheck(){
+		sp.checkSpelling();
+	}
+	public void setSp(SpellChecker sp) {
+		this.sp = sp;
+	}
+	
+	
+}
