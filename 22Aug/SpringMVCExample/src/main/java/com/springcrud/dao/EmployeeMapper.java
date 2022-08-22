acdb103493d22d1de3da6fd221eae35d22367aa4
niitagile <43024343+niitagile@users.noreply.github.com>
2022-08-22 11:54:35 +0530

    Add files via upload

---
+++ 22Aug/SpringMVCExample/src/main/java/com/springcrud/dao/EmployeeMapper.java
@@ -0,0 +1,29 @@
+package com.springcrud.dao;
+
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.springcrud.beans.Employee;
+//RowMapper converts Records from ResultSet to Objects
+public class EmployeeMapper implements RowMapper<Employee>{
+
+	@Override
+	public Employee mapRow(ResultSet rs, int rowNum)throws SQLException{
+		Employee employee =new Employee();
+		
+		employee.setEid(rs.getInt("eid"));
+		employee.setName(rs.getString("name"));
+		employee.setDesignation(rs.getString("designation"));
+		employee.setSalary(rs.getFloat("salary"));
+		return employee;
+	}
+	
+
+	
+	
+
+}
---
+++ 22Aug/SpringMVCExample/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<a href="empform">Add Employee</a>
+<a href="viewemp"> View Employee Details</a>
+</body>
+</html>
