69cd2e7e63aa2ec964e861090a871e75aec1c2e9
niitagile <43024343+niitagile@users.noreply.github.com>
2022-09-16 05:49:34 +0000

    Add files via upload

---
+++ 16Sep/mapexample.ts
@@ -0,0 +1,20 @@
+let ageMapping = new Map();  
+   
+ageMapping.set("Rakesh", 40);  
+ageMapping.set("Abhishek", 25);  
+ageMapping.set("Amit", 30);  
+   
+//Iterate over map keys  
+
+for (let key of ageMapping.keys()) {  
+    console.log("Map Keys= " +key);          
+}  
+//Iterate over map values  
+for (let value of ageMapping.values()) {  
+    console.log("Map Values= " +value);      
+}  
+console.log("The Map Enteries are: ");   
+//Iterate over map entries  
+for (let entry of ageMapping.entries()) {  
+    console.log(entry[0], entry[1]);   
+}  
