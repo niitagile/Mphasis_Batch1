82ed035c82203a83ab88a908636b168dac065cc4
niitagile <43024343+niitagile@users.noreply.github.com>
2022-09-28 04:58:10 +0000

    Add files via upload

---
+++ 28Sep/testngexample/TestAnnotationParameter.java
@@ -0,0 +1,15 @@
+package testngexample;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestAnnotationParameter {
+  @Test
+  
+  @Parameters ({"val1", "val2"})
+  public void Sum(int v1, int v2) {
+  	int finalsum = v1 + v2;
+      System.out.println("The final sum of the given values is " + finalsum);
+  }
+  
+}
